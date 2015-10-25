08f92e7a80246f974d0b9c81f759e8e7075139f7
Havanero <caleb.carvalho@gmail.com>
2015-10-25 09:41:44 +0100

    added junit test cases and refactor

--- market-feature-report/src/main/java/org/jenkinsci/plugins/marketfeaturereport/MarketFeatureBuildAction.java
+++ market-feature-report/src/main/java/org/jenkinsci/plugins/marketfeaturereport/MarketFeatureBuildAction.java
@@ -1,82 +1,36 @@
-/*
- * The MIT License
- *
- * Copyright (c) 2012, Thomas Deruyter, Raynald Briand
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package org.jenkinsci.plugins.marketfeaturereport;
 
 import hudson.model.AbstractBuild;
 import hudson.model.Action;
 import org.jenkinsci.plugins.marketfeaturereport.report.Report;
 import org.kohsuke.stapler.StaplerProxy;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
-/**
- * Class describing action performed on build page.
- */
+
 public class MarketFeatureBuildAction implements Action,
         Serializable, StaplerProxy {
 
-    /**
-     * Id of the class.
-     */
     private static final long serialVersionUID = 1L;
-    /**
-     * URL to access data.
-     */
-    public static final String URL_NAME = "aciResult";
+
     private AbstractBuild<?, ?> build;
     private String result;
     private Report report;
     private ArrayList<ArrayList<String>> fileError;
 
-    /**
-     * Constructor.
-     *
-     * @param build The current build
-     * @param files The current files
-     * @throws InterruptedException         Interruption
-     * @throws ParserConfigurationException Exception in parser configuration
-     * @throws SAXException                 Exception in XML parser
-     * @throws URISyntaxException           Exception in URL
-     * @throws IOException                  Exception with I/Os
-     */
+
     public MarketFeatureBuildAction(final AbstractBuild<?, ?> build,
-            final ArrayList<String> files) throws InterruptedException,
-            ParserConfigurationException, SAXException, URISyntaxException,
+            final ArrayList<String> files) throws InterruptedException, URISyntaxException,
             IOException {
         this.build = build;
         this.report = new Report();
         this.fileError = new ArrayList<>();
 
-        for (int i = 0; i < files.size(); i++) {
-
-            String currentReport = files.get(i);
+        for (String currentReport : files) {
 
             String path = build.getArtifactsDir().getAbsolutePath()
                     + File.separatorChar + currentReport;
@@ -87,75 +41,55 @@ public class MarketFeatureBuildAction implements Action,
                 this.report.addSection(parseCss.result());
             } else {
                 ArrayList<String> list = new ArrayList<>();
-                list.add("#FF0000"); // titleColor
-                list.add("Market Feature Error"); // fieldName
-                list.add("#000000"); // detailColor
-
+                list.add("#FF0000");
+                list.add("Market Feature Error");
+                list.add("#000000");
                 String str = build.getArtifactsDir().getName();
                 str = str.replace("archive", build.getNumber() + "/artifact");
 
                 list.add(str + File.separatorChar + currentReport);
-                // href project page
-
                 str = build.getArtifactsDir().getName();
                 str = str.replace("archive", "artifact");
 
                 list.add(str + File.separatorChar + currentReport);
-                // href build page
-                list.add("Parsing"); // fieldValue
+                list.add("Parsing");
                 fileError.add(list);
             }
         }
 
     }
 
-    /**
-     * Get Report.
-     */
+
     public Report getReport() {
         return report;
     }
 
-    /**
-     * Get Error File.
-     */
     public ArrayList<ArrayList<String>> getFileError() {
         return fileError;
     }
 
-    /**
-     * Get Summary.
-     */
+
     public String getSummary() {
         StringBuilder builder = new StringBuilder();
         return builder.toString();
     }
 
-    /**
-     * Get Details.
-     */
+
     public String getDetails() {
         StringBuilder builder = new StringBuilder();
         return builder.toString();
     }
 
-    /**
-     * Get Result.
-     */
+
     public String getResult() {
         return this.result;
     }
 
-    /**
-     * Get current build.
-     */
+
     AbstractBuild<?, ?> getBuild() {
         return this.build;
     }
 
-    /**
-     * Get Target.
-     */
     public Object getTarget() {
         return this.result;
     }
@@ -172,9 +106,7 @@ public class MarketFeatureBuildAction implements Action,
         return previousResult;
     }
 
-    /**
-     * Get Previous action.
-     */
+
     MarketFeatureBuildAction getPreviousAction() {
         AbstractBuild<?, ?> previousBuild = this.build.getPreviousBuild();
         if (previousBuild != null) {
@@ -183,18 +115,14 @@ public class MarketFeatureBuildAction implements Action,
         return null;
     }
 
-    /**
-     * Get Project Name.
-     */
+
     public String getProjectName() {
         String str = build.getParent().getName();
         str = str.replace(".", "dot");
         return str;
     }
 
-    /**
-     * Get Build Number.
-     */
+
     public int getBuildNumber() {
         return build.getNumber();
     }
@@ -209,21 +137,11 @@ public class MarketFeatureBuildAction implements Action,
         return null;
     }
 
-    /**
-     * The three functions getIconFileName,getDisplayName,getUrlName create a
-     * link to a new page with url : http://{root}/job/{job name}/URL_NAME for
-     * the page of the build.
-     */
     public String getDisplayName() {
-        // return "ACIAction";
+        // return "Build Action";
         return null;
     }
 
-    /**
-     * The three functions getIconFileName,getDisplayName,getUrlName create a
-     * link to a new page with url : http://{root}/job/{job name}/URL_NAME for
-     * the page of the build.
-     */
     public String getUrlName() {
         // return URL_NAME;
         return null;
--- market-feature-report/src/test/java/org/jenkinsci/plugins/marketfeaturereport/TestParseHtmlReport.java
+++ market-feature-report/src/test/java/org/jenkinsci/plugins/marketfeaturereport/TestParseHtmlReport.java
@@ -4,32 +4,35 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class TestParseHtmlReport {
+    private static LinkedHashMap<String, String> summary_table = new LinkedHashMap<>();
+    private static LinkedHashMap<String, String> summary_error_table = new LinkedHashMap<>();
 
-    @Test
-    public void TestParsing() throws IOException {
-        System.out.println("Testing..");
-        File input = new File(
-                "/home/carvcal/IdeaProjects/plugin-project/market-feature-jenkins-plugin/market-feature-report/work/jobs/MM2/workspace/market_feature_passes.html");
-        System.out.println("just name " + input.getName());
-        Document doc = Jsoup.parse(input, "UTF-8");
+
+    @BeforeClass
+    public static void setUp(){
+        //typically the file which has been set to archive
+        File input = new File("work/jobs/Maven1/workspace/market_features.html");
+        Document doc = null;
+        try {
+            doc = Jsoup.parse(input, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert doc != null;
         Element content = doc.getElementById("market-feature-header");
-        System.out.println(content.getElementById("title").text());
         Elements header = content.getElementsByClass("rTableHead");
         Elements failedHeader = content.getElementsByClass("rTableHeadFailed");
         Elements rows = content.getElementsByClass("rTableCell");
         Elements rows_failed = content.getElementsByClass("rTableCellFailed");
-
-        LinkedHashMap<String, String> summary_table = new LinkedHashMap<>();
-        LinkedHashMap<String, String> summary_error_table = new LinkedHashMap<>();
-
         int count_failed = 0, count = 0;
 
         for (Element element : header) {
@@ -44,38 +47,36 @@ public class TestParseHtmlReport {
             ++count_failed;
         }
 
-        for (Map.Entry<String, String> entry : summary_table.entrySet()) {
-            if ((entry.getKey().equals("Failed") || entry.getKey().equals("Errors"))) {
-                switch (entry.getKey()) {
-                    case "Failed":
-                        if (Integer.parseInt(entry.getValue()) != 0) {
-                            System.out.println(
-                                    entry.getKey() + "==" + entry.getValue() + " link Error="
-                                            + summary_error_table
-                                            .get(entry.getKey()));
-                        } else {
-                            System.out.println(entry.getKey() + "==" + entry.getValue());
-                        }
-                        break;
-                    case "Errors":
-                        if (Integer.parseInt(entry.getValue()) != 0) {
-                            System.out.println(
-                                    entry.getKey() + "==" + entry.getValue() + " link Error="
-                                            + summary_error_table
-                                            .get(entry.getKey()));
-                        }
-                        else{
-                            System.out.println(entry.getKey() + "==" + entry.getValue());
-                        }
-                        break;
-                }
+    }
 
-            } else {
-                System.out.println(entry.getKey() + "==" + entry.getValue());
-            }
+    @Test
+    public void testSuccessKeysExtracted()  {
+        Assert.assertNotNull("Failed - No Market Features Key", summary_table.get("Market Features"));
+        Assert.assertNotNull("Failed - No Total Tests Key", summary_table.get("Total Tests"));
+        Assert.assertNotNull("Failed - No Passes Key", summary_table.get("Passes"));
+        Assert.assertNotNull("Failed - No Errors Key", summary_table.get("Errors"));
+        Assert.assertNotNull("Failed - No Failed Key", summary_table.get("Failed"));
+    }
 
-        }
+    @Test
+    public void testSuccessValuesExtracted()  {
+        Assert.assertEquals("Failed - No Market Features Value", summary_table.get("Market Features"), "2");
+        Assert.assertEquals("Failed - No Total Tests Value", summary_table.get("Total Tests"), "8");
+        Assert.assertEquals("Failed - No Passes Value", summary_table.get("Passes"), "6");
+        Assert.assertEquals("Failed - No Errors Value", summary_table.get("Errors"), "0");
+        Assert.assertEquals("Failed - No Failed Value", summary_table.get("Failed"), "2");
+    }
 
+    @Test
+    public void testNoLinkAddedInCaseOfErrorsIsZero(){
+        Assert.assertEquals("Failed - No Errors Value", summary_table.get("Errors"), "0");
+        Assert.assertNull("Error Keys is not empty", summary_error_table.get("Errors"));
+    }
+
+    @Test
+    public void testLinkAddedInCaseOfFailureIsNotZero(){
+        Assert.assertEquals("Failed - No Failed Value", summary_table.get("Failed"), "2");
+        Assert.assertNotNull("Failed Keys is empty", summary_error_table.get("Failed"));
     }
 
 }
